9f5f3d79dd761fac84603bebed288eeda7ab1f53
sanketnighot <sanketnighot25@gmail.com>
2022-10-14 22:22:55 +0530

    0

--- models/TradeData.js
+++ models/TradeData.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const TradeData =new mongoose.Schema(
+const MinuteSchema =new mongoose.Schema(
     {
         Date:{
             type:Date,
@@ -20,6 +20,49 @@ const TradeData =new mongoose.Schema(
         }
     }
 )
+const HourSchema =new mongoose.Schema(
+    {
+        Date:{
+            type:Date,
+        },
+        Open:{
+            type:Number,
 
-module.exports
-=mongoose.model("tradedata", TradeData);
+        },
+        High:{
+            type:Number,
+        },
+        Low:{
+            type:Number,
+        },
+        Close:{
+            type:Number,
+        }
+    }
+)
+const DaySchema =new mongoose.Schema(
+    {
+        Date:{
+            type:Date,
+        },
+        Open:{
+            type:Number,
+
+        },
+        High:{
+            type:Number,
+        },
+        Low:{
+            type:Number,
+        },
+        Close:{
+            type:Number,
+        }
+    }
+)
+const TradeDataMinute = mongoose.model('minute', MinuteSchema);
+const TradeDataHour = mongoose.model('hour', HourSchema);
+const TradeDataDay= mongoose.model('day', DaySchema);
+module.exports = {
+    TradeDataMinute, TradeDataHour, TradeDataDay
+}
