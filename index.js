64ea191a71cebe70fc2f7ca2e5008ba9537ab1b6
sanketnighot <sanketnighot25@gmail.com>
2022-11-07 16:14:25 +0530

    backend update

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const dotenv = require("dotenv")
+const socket = require('socket.io')
 const cors = require('cors');
 const axios = require('axios')
 const { TradeDataHour, TradeDataDay, TradeDataMinute } = require("./models/TradeData");
 const PositionHistory = require('./models/PositionHistory')
 const TokenIssue = require('./models/TokensAddress')
 const { validateAddress } = require("@taquito/utils")
+const signalR = require("@microsoft/signalr");
 const { TezosToolkit } = require("@taquito/taquito");
 const { InMemorySigner } = require("@taquito/signer");
 
@@ -19,25 +21,25 @@ dotenv.config();
 
 const Tezos = new TezosToolkit("https://rpc.ghostnet.teztnets.xyz/");
 
-
+Tezos.setProvider({
+  signer: new InMemorySigner(process.env.PVT_KEY)
+})
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => { console.log("connected to db") }).catch((err) => { console.log(err) });
 
-app.use(cors({ origin: '*' }));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.use(bodyParser.raw());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-
-const PORT = process.env.PORT || 3000;
-
-const server = app.listen(PORT, () => {
-  console.log('server listening at');
-});
-
-
-app.get('/', async (req, res) => {
-  res.send("Root path Zenith API")
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log('Started in 8000 PORT')
 })
 
 const iO = require('socket.io')(server, {
@@ -46,6 +48,14 @@ const iO = require('socket.io')(server, {
   }
 });
 
+
+const senddata = async () => {
+  var x = await TradeDataMinute.find().limit(1).sort({ $natural: -1 }).limit(1)
+    .then((results) => {
+      return results
+    })
+  return x;
+}
 
 iO.on('connection', (client) => {
 
@@ -66,7 +76,7 @@ iO.on('connection', (client) => {
   TradeDataMinute.watch([{ $match: { operationType: { $in: ['insert'] } } }]).
     on('change', data => {
       console.log('Insert action triggered'); //getting triggered thrice
-      client.emit("data3", data.fullDocument.Close);
+      client.emit("data4", data.fullDocument.Close);
     });
   TradeDataMinute.watch([{ $match: { operationType: { $in: ['update'] } } }]).
     on('change', data => {
@@ -74,32 +84,26 @@ iO.on('connection', (client) => {
       client.emit("data4", data.updateDescription.updatedFields.Close);
     });
 });
+console.log('A user connected');
 
-
-const senddata = async () => {
-  var x = await TradeData.find().limit(1).sort({ $natural: -1 }).limit(1)
-    .then((results) => {
-      return results
-    })
-  return x;
-}
 // LEADERBOARD DATA -------------------------------------------------------------------------------------------------------------------
 
 app.get('/leaderboard', async (req, res) => {
-  const result = await PositionHistory.find({}).then(function (data) {
-    return data
-  }).catch(err => console.log(err))
-  result.sort(function (a, b) {
-    return parseFloat(a.Totalpnl) - parseFloat(b.Totalpnl);
-  });
-  res.send(result)
+  try{
+    const result = await PositionHistory.find({}).then(function (data) {
+      return data
+    }).catch(err => console.log(err))
+    result.sort(function (a, b) {
+      return parseFloat(a.Totalpnl) - parseFloat(b.Totalpnl);
+    });
+
+    res.send(result)
+  }catch(err){
+    console.log(err)
+  }
 })
 
-
-
 // POSITION History-------------------------------------------------------------------------------------------------------------------
-
-
 app.post('/positionshistory', async (req, res) => {
   let address = req.body.address;
   const result = await PositionHistory.findOne({ Address: address }).select("Address").lean();
@@ -117,24 +121,23 @@ app.post('/positionshistory', async (req, res) => {
 })
 
 
+// SEND CANDLE DATA -------------------------------------------------------------------------------------------------------------------
 const getData = async () => {
-  
-  const result = TradeDataMinute.find({}).sort({_id: -1}).limit(900,function (data) {
-    return data
+
+  const result = await TradeDataMinute.find({}).sort({ _id: -1 }).limit(100, function (data) {
+    return data.reverse()
   }).catch(err => console.log(err))
+
   return result
 }
 
-
-// SEND CANDLE DATA -------------------------------------------------------------------------------------------------------------------
-
-app.post('/granularity', async (req, res) => {
-
-  if (req.body.granularity == "5minute") {
+app.get('/granularity/', async (req, res) => {
+  console.log(req.query.candle)
+  if (req.query.candle == "5minute") {
     const result = await getData();
     res.send(result.reverse())
   }
-  else if (req.body.granularity == "15minute") {
+  else if (req.query.candle == "15minute") {
     var newdate_Minute = new Date().getMinutes();
     let x = 0
     if(newdate_Minute%15>=5 && newdate_Minute%15<10){
@@ -178,84 +181,19 @@ app.post('/granularity', async (req, res) => {
     }
     res.send(newarr)
   }
-  else if (req.body.granularity == "hour") {
+  else if (req.query.candle == "hour") {
     const result = await TradeDataHour.find({}).then(function (data) {
       return data
     }).catch(err => console.log(err))
-
-
-
-
-    // let newarr = []
-    // for (let i = 0; i < result.length; i = i + 12) {
-    //   let Open = result[i].Open;
-    //   let High = result[i].High;
-    //   let Low = result[i].Low;
-    //   let Close = result[i].Close;
-    //   for (let j = i; j < i + 12; j++) {
-    //     if (j + 1 > result.length) {
-    //       break
-    //     }
-    //     if (High < result[j].High) {
-    //       High = result[j].High
-    //     }
-    //     if (Low > result[j].Low) {
-    //       Low = result[j].Low
-    //     }
-    //     Close = result[j].Close
-    //   }
-    //   let data = {
-    //     Date: result[i].Date,
-    //     Open: Open,
-    //     High: High,
-    //     Low: Low,
-    //     Close: Close,
-    //   }
-    //   newarr.push(data)
-    // }
-
-
     res.send(result)
   }
-  else if (req.body.granularity == "day") {
+  else if (req.query.candle == "day") {
     const result = await TradeDataDay.find({}).then(function (data) {
       return data
     }).catch(err => console.log(err))
-
-    // let newarr = []
-    // for (let i = 0; i < result.length; i = i + 288) {
-    //   let Open = result[i].Open;
-    //   let High = result[i].High;
-    //   let Low = result[i].Low;
-    //   let Close = result[i].Close;
-    //   for (let j = i; j < i + 288; j++) {
-    //     if (j + 1 > result.length) {
-    //       break
-    //     }
-    //     if (High < result[j].High) {
-    //       High = result[j].High
-    //     }
-    //     if (Low > result[j].Low) {
-    //       Low = result[j].Low
-    //     }
-    //     Close = result[j].Close
-    //   }
-    //   let data = {
-    //     Date: result[i].Date,
-    //     Open: Open,
-    //     High: High,
-    //     Low: Low,
-    //     Close: Close,
-    //   }
-    //   newarr.push(data)
-    // }
     res.send(result)
   }
 })
-
-
-
-
 
 
 // SendTestToken-------------------------------------------------------------------------------------------------------------------
